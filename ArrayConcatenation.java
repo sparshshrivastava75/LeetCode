9452cbf4b223510576e7f48238270bf3b6d97079
sparshshrivastava75 <43951895+sparshshrivastava75@users.noreply.github.com>
2023-02-08 00:39:14 +0530

    Add files via upload

---
+++ ArrayConcatenation.java
@@ -0,0 +1,13 @@
+class ArrayConcatenation {
+    public int[] getConcatenation(int[] nums) {
+        int ans[]=new int[nums.length*2];
+        for(int i=0;i<nums.length;i++){
+            ans[i]=nums[i];
+            ans[nums.length+i]=nums[i];
+        }
+    
+        return ans;
+        
+        
+    }
+}q
